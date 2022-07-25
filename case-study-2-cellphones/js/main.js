15a9959ec86d15fc7962647292a3f46906ad0649
Trần Thị Thúy <tranthuy0611bacninh@gmail.com>
2022-07-25 19:28:34 +0700

    update case-study-2 5

--- case-study-2-cellphones/js/main.js
+++ case-study-2-cellphones/js/main.js
@@ -6,6 +6,7 @@ const btnControls = document.querySelectorAll('.main-carousel .carousel-control'
 const btnCarouselIndis = document.querySelectorAll('.main-carousel .carousel-indicators div')
 const textCarouselIndis = document.querySelectorAll('.main-carousel .carousel-indicators span')
 const menuFooterItems = document.querySelectorAll('.menu-footer-item')
+const boxChoices = document.querySelectorAll('.version')
 textCarouselIndis.forEach(function(textCarouselIndi){
     textCarouselIndi.style.width = wMainCarousel / 5 + 'px'
 })
@@ -26,11 +27,6 @@ function scrollToView(){
     }
     , 300)
 }
-document.querySelector('.carousel-indicators div.active').scrollIntoView({
-    behavior: 'smooth',
-    block: 'nearest'
-});
-
 
 // Set backgroud-color menu-footer-item
 let colorList = ['#ffa3a3', '#ffb8b8','#ff94eb', '#e0b3ff','#c6d8fb','#85ffb1','#f5d63d', '#fda363','#ff6666']
@@ -44,4 +40,19 @@ menuFooterItems.forEach(function(menuFooterItem){
         color += hex[Math.floor(Math.random()*16)]
     }
     menuFooterItem.style.backgroundColor = color
+})
+
+// Css actice btn box-choice
+boxChoices.forEach(function(boxChoice){
+    boxChoice.addEventListener("click", addActive)
+    function addActive(){
+        let boxChoicesActive = document.querySelector('.version.active')
+        if(boxChoicesActive){
+            if(boxChoice.classList.contains("active")){
+            } else{
+                boxChoice.classList.add("active")
+                boxChoicesActive.classList.remove("active")
+            }
+        }
+    }
 })
